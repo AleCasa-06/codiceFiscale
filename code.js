0e9a981760f5fa5ea1f76bde1cd0fdc4b7525a03
AleCasa-06 <115339459+AleCasa-06@users.noreply.github.com>
2023-01-23 20:45:17 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,21 @@
+function estraiDaNome(){
+
+}
+
+
+
+
+
+function estraiDaData(giorno){
+    
+    if(document.getElementById("F").checked){
+        giorno += 40; 
+        
+    } 
+    return giorno; 
+}
+
+function main(){
+    
+}
+
